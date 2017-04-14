91bb26d3b81653d489111fd19dfa4fe5073d7fe5
Girard Lionel <lgirardap@gmail.com>
2017-04-14 04:57:42 +0100

    Created sender entity + form and add method

---
+++ src/Adena/MailBundle/Form/SenderType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Adena\MailBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SenderType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',       TextType::class)
+            ->add('email',      EmailType::class)
+            ->add('password',   PasswordType::class)
+            ->add('active',     CheckboxType::class)
+            ->add('save',       SubmitType::class);
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Adena\MailBundle\Entity\Sender'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'adena_mailbundle_sender';
+    }
+
+
+}
