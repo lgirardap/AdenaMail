49323ea72c9667741269777b5585ecc80960fc2c
Olivier "Growiel" Girard <girardol182@gmail.com>
2017-04-17 04:25:02 +0100

    Adding Datasource entity

---
+++ src/Adena/MailBundle/Entity/Datasource.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Adena\MailBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Datasource
+ *
+ * @ORM\Table(name="datasource")
+ * @ORM\Entity(repositoryClass="Adena\MailBundle\Repository\DatasourceRepository")
+ */
+class Datasource
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="host", type="string", length=255)
+     */
+    private $host;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="port", type="integer", length=5)
+     */
+    private $port;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="database_name", type="string", length=255)
+     */
+    private $databaseName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set host
+     *
+     * @param string $host
+     *
+     * @return Datasource
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+
+    /**
+     * Get host
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * Set port
+     *
+     * @param string $port
+     *
+     * @return Datasource
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+
+        return $this;
+    }
+
+    /**
+     * Get port
+     *
+     * @return string
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Datasource
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Datasource
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set databaseName
+     *
+     * @param string $databaseName
+     *
+     * @return Datasource
+     */
+    public function setDatabaseName($databaseName)
+    {
+        $this->databaseName = $databaseName;
+
+        return $this;
+    }
+
+    /**
+     * Get databaseName
+     *
+     * @return string
+     */
+    public function getDatabaseName()
+    {
+        return $this->databaseName;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Datasource
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
