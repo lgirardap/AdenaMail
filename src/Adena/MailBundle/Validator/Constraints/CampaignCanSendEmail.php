a3e11d0329fe3c9b05e693f8374f10aaa41d27c8
Olivier "Growiel" Girard <girardol182@gmail.com>
2017-06-28 07:05:18 +0100

    Check that a campaign has compatible mailing lists and template before saving the form

---
+++ src/Adena/MailBundle/Validator/Constraints/CampaignCanSendEmail.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Adena\MailBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+* @Annotation
+*/
+class CampaignCanSendEmail extends Constraint
+{
+    public $message = 'The {{ error_message }}. Please update the MailingList to add it, use a different MailingList or update the Email template.';
+
+    public function validatedBy()
+    {
+        return get_class($this).'Validator';
+    }
+
+    // Makes this a "class level" constraint. Not designed to be used on properties, it would make no sense.
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
