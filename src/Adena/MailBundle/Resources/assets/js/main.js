e26d3f4076488764f8b84cf30fdaebe3ba6d3ecd
Girard Lionel <lgirardap@gmail.com>
2017-07-13 02:52:38 +0100

    Started POC with Webpack Encore

---
+++ src/Adena/MailBundle/Resources/assets/js/main.js
@@ -0,0 +1,4 @@
+test1 = require("jquery");
+window.test = require("brace");
+formField = require("./form_fields");
+
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+var Encore = require('@symfony/webpack-encore');
+var path = require('path');
+
+Encore
+// directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // will output as web/build/app.js
+    .addEntry('app', './src/Adena/MailBundle/Resources/assets/js/main.js')
+
+    // will output as web/build/global.css
+    .addStyleEntry('global', './src/Adena/MailBundle/Resources/assets/css/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+// create hashed filenames (e.g. app.abc123.css)
+// .enableVersioning()
+;
+
+var config = Encore.getWebpackConfig();
+config.resolve.alias.ace = path.resolve(__dirname, 'src/Adena/MailBundle/Resources/public/lib/ace/ace');
+
+// export the final configuration
+module.exports = config;
